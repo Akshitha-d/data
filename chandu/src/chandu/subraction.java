c7b25d5ac99799a4f4751f0d05c7636ce3703999
akshitha <akshitha209@gmail.com>
2020-05-04 21:07:38 +0530

    subraction of two numbers

---
+++ chandu/src/chandu/subraction.java
@@ -0,0 +1,15 @@
+package chandu;
+
+import java.util.Scanner;
+
+public class subraction {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("enter the two numbers");
+		Scanner in=new Scanner(System.in);
+		int a=in.nextInt();
+		int b=in.nextInt();
+		System.out.println("difference="+(a-b));
+	}
+
+}
